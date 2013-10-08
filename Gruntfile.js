e01312d87b39dc849acd21aee859f2a1f8b112ad
Lyubomir Georgiev <lyubomir.georgiev@telerik.com>
2013-10-08 14:47:31 +0300

    setting simple grunt configuration

---
+++ Gruntfile.js
@@ -0,0 +1,23 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.jsFile %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: '<%= pkg.projectPath %>/src/<%= pkg.jsFile %>.js',
+        dest: '<%= pkg.projectPath %>/build/<%= pkg.jsFile %>.min.js'
+      }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  // Default task(s).
+  grunt.registerTask('default', ['uglify']);
+
+};
---
+++ testdir/src/test.js
@@ -0,0 +1,8 @@
+var testFunction = function() {
+  var tfThis = 0,
+      tfFunction = 2,
+      tfDoes = 4,
+      tfNothing = 8;
+
+      return tfThis + tfFunction + tfDoes + tfNothing;
+}
