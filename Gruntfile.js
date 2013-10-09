894559afb79098099df10042a52e8b93f70c9ad6
Lyubomir Georgiev <lyubomir.georgiev@telerik.com>
2013-10-09 17:35:54 +0300

    Added great files mapping for uglify - matches predefined and maps them to a new files with the same names. also excludes all .jss within the subfolders... love it<3

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,17 +1,24 @@
-module.exports = function(grunt) {
+  module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+    // Project configuration.
+    grunt.initConfig({
+      pkg: grunt.file.readJSON('package.json'),
 
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.jsFile %> <%= grunt.template.today("yyyy-mm-dd HH:ss") %> */\n',
-        report: 'min'
-      },
-      build: {
-        src: '<%= pkg.projectPath %>/src/<%= pkg.jsFile %>.js',
-        dest: '<%= pkg.projectPath %>/build/<%= pkg.jsFile %>.min.js'
+      uglify: {
+        options: {
+          banner: '/*! <%= pkg.jsFile %> <%= grunt.template.today("yyyy-mm-dd HH:ss") %> */\n',
+          report: 'min'
+        },
+        makeatry: {
+          files: [
+          {
+          expand: true,                         // Enable dynamic expansion.
+          cwd: '<%= pkg.projectPath %>/src/',   // Src matches are relative to this path.
+          src: ['/*.js'],                       // Actual pattern(s) to match.
+          dest: '<%= pkg.projectPath %>/build/',// Destination path prefix.
+          ext: '.min.js',                       // Dest filepaths will have this extension.
+        },
+        ],
       }
     },
 
@@ -21,17 +28,17 @@ module.exports = function(grunt) {
       },
     },
 
-   csslint: {
-    options: {
-      csslintrc: '.config/.csslintrc'
-    },
-    strict: {
+    csslint: {
       options: {
-        import: 2
+        csslintrc: '.config/.csslintrc'
       },
-      src: ['<%= pkg.projectPath %>/src/*.css']
-    }
-  },
+      strict: {
+        options: {
+          import: 2
+        },
+        src: ['<%= pkg.projectPath %>/src/*.css']
+      }
+    },
 
     watch: {
       options: {
@@ -39,23 +46,23 @@ module.exports = function(grunt) {
       },
       scripts: {
        tasks: ['jshint', 'uglify'],
-       files: ['<%= pkg.projectPath %>/src/*.js'],
+       files: ['<%= pkg.projectPath %>/src/**/*.js'],
      },
    }
 
-});
+ });
 
-  // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-csslint');
+    // Load the plugins.
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-csslint');
 
-  // Default task(s).
-  grunt.registerTask('default', ['watch']);
+    // Default task(s).
+    grunt.registerTask('default', ['watch']);
 
-  grunt.event.on('watch', function(action, filepath, target) {
-    //grunt.log.writeln(target + ': ' + filepath + ' has ' + action);
-  });
+    grunt.event.on('watch', function(action, filepath, target) {
+      //grunt.log.writeln(target + ': ' + filepath + ' has ' + action);
+    });
 
-};
+  };
